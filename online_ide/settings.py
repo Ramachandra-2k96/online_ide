c9301cd5a61850d3047bb84a38a3df33446b65d4
Ramachandra <ramachandraudupa.sangamone@gmail.com>
2024-02-21 22:44:32 +0530

    update

--- online_ide/settings.py
+++ online_ide/settings.py
@@ -113,10 +113,10 @@ USE_I18N = True
 USE_TZ = True
 
 import os
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     # Add your app's static directory
     os.path.join(BASE_DIR, 'ide_app', 'static'),
