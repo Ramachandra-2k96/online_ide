ca6bee420cd78e10a712c286d1e1bce876214b61
Ramachandra <ramachandraudupa.sangamone@gmail.com>
2024-02-22 18:50:51 +0530

    updated

--- ide_app/views.py
+++ ide_app/views.py
@@ -4,6 +4,7 @@ from decouple import config
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
+from online_ide.settings import BASE_DIR
 def home(request):
     return render(request,"index.html")
 
@@ -15,16 +16,15 @@ def compile_code(request):
         code = request.POST.get('code', '')
         output, error = "", ""
         if language == "python":
-            random_filename = os.path.join("temp", f"{os.urandom(16).hex()}.{language}")
+            random_filename = os.path.join(os.path.join(BASE_DIR,"temp"), f"{os.urandom(16).hex()}.{language}")
             with open(random_filename, "w") as program_file:
                 program_file.write(code)
-            python_executable = "venv/bin/python"  # Update this with the correct path
+            python_executable = os.path.join(BASE_DIR,"venv/bin/python")  
             command = [python_executable, random_filename]
             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output, error = process.communicate()
             output = output.decode('utf-8')
             error = error.decode('utf-8')
-
             # Clean up: Delete the temporary file
             os.remove(random_filename)
 
